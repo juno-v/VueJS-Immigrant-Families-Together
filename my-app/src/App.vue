800d29b0670938d865e59d9c4921eb0ed0528cae
Juno Vue <Juno.Vue@Hotmail.com>
2019-08-01 19:40:03 -0500

    turning a to do list example into personal project

---
+++ my-app/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <h1> Juno's awesome blog </h1>
+      <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    </div>
+     <Container /> 
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Container from "./components/Container.vue"; 
+
+export default {
+  name: 'app',
+  components: {
+    //HelloWorld
+    Container, 
+  }
+}
+</script>
+
+
+/* scoped means only accessed in this component */
+<style scope>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; */
+}
+
+/* .container-inner {
+  background-color: green; 
+} */
+</style>
---
+++ my-app/src/components/Admin/AdminLandingPage.vue
@@ -0,0 +1,50 @@
+<template>
+    <center>
+                    <div>
+                        <h1>MENU</h1>
+                    </div>
+
+                    <button className="adminMenuButtons"
+                    v-on:click="viewEvents"
+                    >UPCOMING EVENTS</button> 
+
+                    <button className="adminMenuButtons"
+                    v-on:click="viewCases"
+                    >CASE MANAGEMENT </button> 
+
+                    <button className="adminMenuButtons"
+                    v-on:click="viewVolunteer"
+                    >VOLUNTEERS</button> 
+
+                    <button className="adminMenuButtons"
+                    v-on:click="registerVolunteer"
+                    >REGISTER VOLUNTEER</button> 
+                   
+                </center>
+</template>
+
+<script>
+export default {
+    methods: {
+        viewEvents() {
+            console.log(`view events`);
+        },
+        viewCases() {
+            console.log(`view cases`);
+        }, 
+        viewVolunteer() {
+            console.log(`view volunteers`);
+        },
+        registerVolunteer() {
+            console.log(`register volunteers`);
+        },
+
+
+    }
+  
+}
+</script>
+
+<style>
+
+</style>
