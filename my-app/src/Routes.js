0fcca760f7dc1c7fd672c940ac00609598e807b8
Juno Vue <Juno.Vue@Hotmail.com>
2019-08-11 04:19:34 -0500

    created routes for Case Management landing as well as a src folder with .vue files

--- my-app/src/Routes.js
+++ my-app/src/Routes.js
@@ -5,13 +5,26 @@ import Cases from "./components/Admin/LandingPage/Cases.vue";
 import Volunteers from "./components/Admin/LandingPage/Volunteers.vue";
 import RegisterVolunteer from "./components/Admin/LandingPage/RegisterVolunteer.vue";
 
+/* imports for Case Management landing */
+import ViewCase from "./components/Admin/CaseManagement/ViewCase.vue";
+import EditCase from "./components/Admin/CaseManagement/EditCase.vue";
+import AddCase from "./components/Admin/CaseManagement/AddCase.vue";
+import CloseCase from "./components/Admin/CaseManagement/CloseCase.vue";
+
 export default [
     /* imports for Admin landing page */
     {path: '/', component: LandingPage},
     {path: '/events', component: Events},
     {path: '/cases', component: Cases},
     {path: '/volunteers', component: Volunteers},
-    {path: '/register-volunteer', component: RegisterVolunteer}
+    {path: '/register-volunteer', component: RegisterVolunteer},
+
+    /* imports for Case Management landing */
+    {path: '/view-case', component: ViewCase},
+    {path: '/edit-case', component: EditCase},
+    {path: '/add-case', component: AddCase},
+    {path: '/close-case', component: CloseCase},
+
 
     
 ]
