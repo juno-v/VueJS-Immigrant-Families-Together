b1151492566d1cac9fc3101d8bee50bea9d8d696
Juno Vue <Juno.Vue@Hotmail.com>
2019-08-11 13:06:43 -0500

    added route and visual for volunteer landing

--- my-app/src/Routes.js
+++ my-app/src/Routes.js
@@ -1,5 +1,5 @@
 /* imports for Admin landing page */
-import LandingPage from "./components/Admin/LandingPage/LandingPage.vue"; 
+import AdminLanding from "./components/Admin/LandingPage/AdminLanding.vue"; 
 import Events from "./components/Admin/LandingPage/Events.vue";
 import Cases from "./components/Admin/LandingPage/Cases.vue";
 import Volunteers from "./components/Admin/LandingPage/Volunteers.vue";
@@ -21,9 +21,12 @@ import AidForm from "./components/Admin/CreateCase/AidForm.vue";
 import BondForm from "./components/Admin/CreateCase/BondForm.vue"; 
 import LegalForm from "./components/Admin/CreateCase/LegalForm.vue";
 
+/* imports for Volunteer */
+import LandingPage from "./components/Volunteer/LandingPage.vue"; 
+
 export default [
     /* imports for Admin landing page */
-    {path: '/admin-landing', component: LandingPage},
+    {path: '/admin-landing', component: AdminLanding},
     {path: '/events', component: Events},
     {path: '/cases', component: Cases},
     {path: '/volunteers', component: Volunteers},
@@ -36,7 +39,6 @@ export default [
     {path: '/close-case', component: CloseCase},
 
     /* imports for Creating Case Forms */
-    {path: '/bio-form', component: BioForm},
     {path: '/children-form', component: ChildrenForm},
     {path: '/medical-form', component: MedicalForm},
     {path: '/school-form', component: SchoolForm},
@@ -45,6 +47,9 @@ export default [
     {path: '/bond-form', component: BondForm},
     {path: '/legal-form', component: LegalForm},
     
+
+    /* imports for Volunteer */
+    {path: '/volunteer-landing', component: LandingPage},
 
 
     
---
+++ my-app/src/components/Volunteer/LandingPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <center> 
+    <h1> CASES </h1>
+    <table>
+        <thead>
+            <tr>
+                <th>LAST NAME</th>
+                <th>ID NUMBER</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    {cases.last_name}
+                </td>
+                <td>
+                    {cases.case_id}
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</center>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
