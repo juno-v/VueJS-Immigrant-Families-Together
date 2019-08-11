96ce4e324c9da710c0a29ba1c6ba05fe2d304940
Juno Vue <Juno.Vue@Hotmail.com>
2019-08-11 03:07:27 -0500

    set up routes for landing page

--- my-app/src/Routes.js
+++ my-app/src/Routes.js
@@ -1,7 +1,13 @@
-import AdminLandingPage from "./components/Admin/AdminLandingPage.vue";
-import Events from "./components/Admin/Events/Events.vue"; 
+
+/* imports for Admin landing page */
+import LandingPage from "./components/Admin/LandingPage/LandingPage.vue"; 
+import Events from "./components/Admin/LandingPage/Events.vue"
 
 export default [
-    {path: '/', component: AdminLandingPage},
-    {path: '/events', component: Events}
+    /* imports for Admin landing page */
+    {path: '/', component: LandingPage},
+    {path: '/events', component: Events},
+    {path: '/register-volunteer', component: Events},
+    {path: '/volunteers', component: Events},
+    {path: '/cases', component: Events},
 ]
---
+++ my-app/src/components/Admin/LandingPage/LandingPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <center>
+        <div>
+            <h1>MENU</h1>
+        </div>
+
+        <router-link to="/events">
+            <button class="adminMenuButtons">UPCOMING EVENTS</button>
+        </router-link> <br/>
+        
+        <router-link to="/cases">
+            <button class="adminMenuButtons">CASE MANAGEMENT</button>
+        </router-link> <br/>
+        <router-link to="/volunteers">
+            <button class="adminMenuButtons">VOLUNTEERS</button>
+        </router-link> <br/>
+        <router-link to="/register-volunteer">
+            <button class="adminMenuButtons">REGISTER VOLUNTEERS</button>
+        </router-link> <br/>
+        
+    </center>
+</template>
+
+<script>
+export default {
+    methods: {
+     
+    }
+}
+</script>
+
+<style>
+
+</style>
